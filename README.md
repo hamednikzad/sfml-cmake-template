59889de9993ea7682e2d9ae0cb263973d2e71a70
Hamed Nikzad <nikzad1603@gmail.com>
2023-12-09 23:35:34 +0330

    Chane CMakeLists.txt

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-SFML CMake Template
+# SFML CMake Template
 
 Template for start coding with SFML
+
+## Setup
+Download SFML from <a href="doc:introduction" target="_blank">Introduction</a> and change path in CMakeLists.txt: 
+</br>
+```set(SFML_PATH C:/OpenGl/Engines/SFML-2.6.1)```
--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,23 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        sf::Event event;
+        while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {
                 window.close();
-            }
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
     }
+
+    return 0;
 }
